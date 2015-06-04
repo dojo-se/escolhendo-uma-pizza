72a13d4f90eda8dc65d5a2de055af8200d237e4c
Fernando Mendonça de Almeida <fernando.m.al.91@gmail.com>
2015-06-03 21:46:10 -0300

    Final do Dojo, esquecemos dos commits intermediarios :( Pedimos perdao pelo vacilo!

--- src/hello.js
+++ src/hello.js
@@ -1,5 +1,34 @@
-function greet(name) {
-	return 'Hello, ' + name + '!';
-}
+var notas = function(nome, notas){
+	var nomesSabores = ['Marguerita', 'Quatro queijos', 'Escarola', 'Portuguesa', 'Frango+Catupiry', 'Napolitana'];
+	// var notasJson =
+	// 	{ 'Roberto' :
+	// 		{
+	// 			'Marguerita' : 4,
+	// 			'Quatro queijos' : 5,
+	// 			'Escarola' : 4,
+	// 			'Portuguesa' : 5,
+	// 			'Frango+Catupiry' : 4,
+	// 			'Napolitana' : 3
+	// 		}
+	// 	};
+	var notasJson = {};
 
-module.exports.greet = greet;
+	notasJson[nome] = {};
+
+	for(var i=0; i<nomesSabores.length; i++) {
+		notasJson[nome][nomesSabores[i]] = notas[i];
+	}
+
+	return notasJson;
+};
+
+var encontrarAmigo = function(eu, amigos){
+		var pontuacoes = {};
+		for(var i = 0; i < amigos.length; i++){
+			var diff = eu
+		}
+	 return amigos[0];
+};
+
+module.exports.notas = notas;
+module.exports.encontrarAmigo = encontrarAmigo;
--- test/hello.spec.js
+++ test/hello.spec.js
@@ -1,8 +1,60 @@
 var assert = require('assert');
-var hello = require('../src/hello');
+var pessoas = require('../src/hello');
 
-describe('greeter', function(){
-	it('should say Hello to the World', function() {
-		assert.equal(hello.greet('World'),'Hello, World!');
+describe('Montagem de Lista de Notas', function(){
+	it('deve montar uma lista de sabores para Roberto', function() {
+		assert.deepEqual(pessoas.notas("Roberto",[4,5,4,5,4,3]),
+			{ 'Roberto' :
+				{
+					'Marguerita' : 4,
+					'Quatro queijos' : 5,
+					'Escarola' : 4,
+					'Portuguesa' : 5,
+					'Frango+Catupiry' : 4,
+					'Napolitana' : 3
+				}
+			}
+		);
+	});
+
+	it('deve montar uma lista de sabores para Erick', function() {
+		assert.deepEqual(pessoas.notas("Erick",[1,2,3,4,5,5]),
+			{ 'Erick' :
+				{
+					'Marguerita' : 1,
+					'Quatro queijos' : 2,
+					'Escarola' : 3,
+					'Portuguesa' : 4,
+					'Frango+Catupiry' : 5,
+					'Napolitana' : 5
+				}
+			}
+		);
 	});
 });
+
+describe('Escolhendo o sabor de Pizza', function() {
+	// Para poder acessar no beforeEach e nos 'its' da vida
+	// var nomeVariavel;
+	var notas;
+	var jackson;
+	var raphael;
+	var kley;
+	var fernando;
+
+	beforeEach(function() {
+		 jackson = pessoas.notas("Jackson", [1,1,2,3,4,5]);
+		 raphael = pessoas.notas("Raphael", [1,2,3,4,5,5]);
+		 kley = pessoas.notas("Kley", [5,4,3,2,1,1]);
+		 fernando = pessoas.notas("Fernando", [5,5,4,3,2,1]);
+		 notas = [jackson, raphael, kley, fernando];
+	});
+
+	it('Deve Retornar Unico Parceiro', function() {
+		assert.deepEqual(pessoas.encontrarAmigo(jackson, [raphael]), raphael);
+	});
+
+	it('Deve Retornar Unico Parceiro dentre 2 amigos', function() {
+		assert.deepEqual(pessoas.encontrarAmigo(jackson, [fernando,raphael]), raphael);
+	});
+})
